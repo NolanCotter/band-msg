d07e0ac176d089ccfcb2af77bf1fca1084952fb7
ayobro1 <nolan.cotter@icloud.com>
2026-03-16 21:37:23 -0700

    URGENT BUILD FIX: Remove extra closing div tag from Calendar.svelte

--- svelte-src/lib/components/Calendar.svelte
+++ svelte-src/lib/components/Calendar.svelte
@@ -324,12 +324,3 @@
       {/if}
     </div>
   </div>
-  </div>
-  <!-- svelte-ignore a11y_click_events_have_key_events -->
-  <!-- svelte-ignore a11y_no_static_element_interactions -->
-  <div
-    class="fixed inset-0 bg-black/80 z-[200]"
-    style="padding-top: env(safe-area-inset-top);"
-    on:click={onClose}
-  ></div>
-</div>
